040487654a5be892076368502c067c04355f29bb
sklbz <sokolbozanic@gmail.com>
2023-12-16 22:04:49 +0100

    started the programm

--- src/generate_fn.rs
+++ src/generate_fn.rs
@@ -1,7 +1,9 @@
-pub fn generate_fn(first_coefficient: i32, first_derivative_root: i32, second_derivative_root:i32, /* first_value: i32, last_value: i32, */ first_extremum: i32, second_extremum: i32) {
+pub fn generate_fn(first_coefficient: i32, first_derivative_root: i32, second_derivative_root:i32, /* first_value: i32, last_value: i32, */ first_extremum: i32, second_extremum: i32, first_derivative_coefficient: i32) {
   let sum_derivative_roots: i32 = first_derivative_root + second_derivative_root;
   let product_derivative_roots: i32 = first_derivative_root * second_derivative_root;
-  
+
+  let second_derivative_coefficient: i32 = -first_derivative_coefficient * product_derivative_roots; // calculate b'
+  let third_derivative_coefficient: i32 = first_derivative_coefficient;
   let mut _b: i32;
   let mut _c: i32;
   let mut _d: i32;
