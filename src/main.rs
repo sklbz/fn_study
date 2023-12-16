11bc5480b2e091273d5c2784df57346d45e00d36
sklbz <sokolbozanic@gmail.com>
2023-12-16 22:54:29 +0100

    working program

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
 mod generate_fn;
 use generate_fn::generate_fn;
-use rand::Rng;
+use rand::{
+    self,
+    distributions::{Distribution, Uniform},
+    Rng,
+};
+
+struct Filter<Dist, Test> {
+    dist: Dist,
+    test: Test,
+}
+
+impl<T, Dist, Test> Distribution<T> for Filter<Dist, Test>
+where
+    Dist: Distribution<T>,
+    Test: Fn(&T) -> bool,
+{
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> T {
+        loop {
+            let x = self.dist.sample(rng);
+            if (self.test)(&x) {
+                return x;
+            }
+        }
+    }
+}
+
 
 fn main() {
-    generate_fn(-1,1,1,2,1);
+    let mut rng = rand::thread_rng();
+    let dist = Filter {
+        dist: Uniform::new_inclusive(-10, 10),
+        test: |x: &_| (x != &0) & (x != &2),
+    };
+
+
+    generate_fn(rng.sample(&dist),rng.sample(&dist),rng.sample(&dist),rng.sample(&dist),rng.sample(&dist));
 }
