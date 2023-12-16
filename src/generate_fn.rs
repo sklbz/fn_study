3ccfb1bb54c39080a52f0f9974c6dbc5fbc3c8f4
sklbz <sokolbozanic@gmail.com>
2023-12-16 13:46:22 +0100

    v

--- src/generate_fn.rs
+++ src/generate_fn.rs
@@ -1,6 +1,10 @@
-pub fn generate_fn(first_coefficient: i32, first_derivative_root: i32, second_derivative_root:i32, first_value: i32, last_value: i32, first_extremum: i32, second_extremum: i32) {
+pub fn generate_fn(first_coefficient: i32, first_derivative_root: i32, second_derivative_root:i32, /* first_value: i32, last_value: i32, */ first_extremum: i32, second_extremum: i32) {
+  let sum_derivative_roots: i32 = first_derivative_root + second_derivative_root;
+  let product_derivative_roots: i32 = first_derivative_root * second_derivative_root;
+  
   let mut _b: i32;
   let mut _c: i32;
-  let mut _c: i32;
-  print!("{first_coefficient}\n{first_derivative_root}\n{second_derivative_root}\n{first_value}\n{last_value}\n{first_extremum}\n{second_extremum}\n");
+  let mut _d: i32;
+  let mut _e: i32;
+  //print!("{first_coefficient}\n{first_derivative_root}\n{second_derivative_root}\n{first_value}\n{last_value}\n{first_extremum}\n{second_extremum}\n");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod generate_fn;
 use generate_fn::generate_fn;
 
 fn main() {
-    generate_fn(-1,1,1,1,1, 1, 1);
+    generate_fn(-1,1,1,1,1);
 }
